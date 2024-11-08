f5003ebae91fe3abbed9f0a22997c0d005bd4d3a
Geun-Oh <kandy1002@naver.com>
2024-11-08 10:58:28 +0900

    feat: Add default nix configuration

---
+++ modules/shared/programs/nix/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+let
+  nix-activate = pkgs.writeShellScriptBin "nix-activate" ''
+    set -euo pipefail
+
+    CURRENT_DIR="$(pwd)"
+    if [ -f "$CURRENT_DIR/flake.nix" ]; then
+      echo "use flake ." > .envrc
+      exit 0
+    fi
+
+    NAME="$(basename $(dirname $CURRENT_DIR))/$(basename $CURRENT_DIR)"
+    NIX_ACTIVATE_ROOT="''${NIX_ACTIVATE_ROOT:-~/conf/nix/nixos-flakes}"
+
+    echo "use flake $NIX_ACTIVATE_ROOT/$NAME" > .envrc
+  '';
+in
+{
+  home.packages = with pkgs; [
+    nil
+    nixfmt-rfc-style
+    nix-activate
+  ];
+}
