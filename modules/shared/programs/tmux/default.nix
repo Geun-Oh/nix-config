381776d7eb7cd2516a325ebbd1796e795acab219
Geun-Oh <kandy1002@naver.com>
2024-11-08 10:59:39 +0900

    feat: Add default tmux configuration

---
+++ modules/shared/programs/tmux/default.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    extraConfig = ''
+            set -ga terminal-overrides ',*-256color*:Tc'
+
+      # ctrl+b highlight
+      set -g status-right '#{?client_prefix,#[reverse] ^B #[noreverse],} | %a %Y-%m-%d %H:%M'
+
+      # vim binding
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # open new window/pane from current path
+      bind '"' split-window -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+
+      # bottom status bar color
+      set -g status-bg green
+      set -g status-fg black
+    '';
+  };
+}
