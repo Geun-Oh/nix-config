843110333b56c7249aa9285d24ce2382a0cf50c2
Geun-Oh <kandy1002@naver.com>
2024-11-12 17:04:51 +0900

    fix: Delete unavailable option in k8s (orbstack)

--- modules/shared/programs/container/default.nix
+++ modules/shared/programs/container/default.nix
@@ -16,12 +16,14 @@ in
       podman
       podman-compose
       qemu
-    ]
-    ++ lib.optionals isDarwin [ orbstack ]
-    ++ lib.optionals isLinux [
       docker
       docker-compose
     ];
+    # ++ lib.optionals isDarwin [ orbstack ]
+    # ++ lib.optionals isLinux [
+    #   docker
+    #   docker-compose
+    # ];
 
   home.sessionVariables = {
     DOCKER_DEFAULT_PLATFORM = "linux/amd64";
